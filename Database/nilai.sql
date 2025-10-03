1f91b57721a44cb6d9b00179fe7dc334e9432695
Nova Fajri <novafajri20@gmail.com>
2025-10-03 21:41:04 +0700

    Insert Data tabel siswa dan nilai

---
+++ Database/nilai.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.nilai (siswa_id,mata_pelajaran,nilai) VALUES
+	 (1,'Matematika',85),
+	 (2,'Bahasa Inggris',90),
+	 (3,'Fisika',75),
+	 (4,'Kimia',72),
+	 (5,'Biologi',85);
---
+++ Database/siswa.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.siswa (nama,umur,jurusan) VALUES
+	 ('Andi',16,'IPA'),
+	 ('Budi',17,'IPS'),
+	 ('Candi',16,'IPS'),
+	 ('Dendi',16,'Bahasa'),
+	 ('Endo',17,'IPA');
